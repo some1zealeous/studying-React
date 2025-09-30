d5de8e900faaebc39b327b09c819dfe480714fbc
MD KAIF <mdkaif7625@gmail.com>
2025-09-30 12:59:03 +0530

    vjs in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+const Nerd = {
+  hobby: 'interest',
+  interest: 'everything',
+  everything: 'shit',
+  shit: 'Israel'
+}
+
 function DisplayInfo() {
 
   const name = 'SomeOne';
@@ -8,6 +15,9 @@ function DisplayInfo() {
 
     <label htmlFor="search">Search: </label>
     <input type="text" id="search" />
+
+    <h1>{"My hobby is my " + Nerd.hobby + " and my " + Nerd.hobby + " is in " + Nerd.interest } </h1>
+    <h2>{Nerd.interest + " is a " + Nerd.everything + " and " + Nerd.everything + " is " + Nerd.shit} </h2>
     </>
   )
 };
