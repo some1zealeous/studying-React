f80e94b150e7655fd3450c97915f400de50d86b5
MD KAIF <mdkaif7625@gmail.com>
2025-09-30 13:03:56 +0530

    js interpolation

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ const Nerd = {
   shit: 'Israel'
 }
 
+function getTitle(title) {
+  return title
+}
+
 function DisplayInfo() {
 
   const name = 'SomeOne';
@@ -12,10 +16,15 @@ function DisplayInfo() {
     <>
     <h1>Hi, {name}!</h1>
     <p>Grab your coffee, and Let's get Started!</p>
+    <br /><br />
 
+    <h1>{getTitle('React')}</h1>
+
+    <br /><br />
     <label htmlFor="search">Search: </label>
     <input type="text" id="search" />
 
+    <br /><br />
     <h1>{"My hobby is my " + Nerd.hobby + " and my " + Nerd.hobby + " is in " + Nerd.interest } </h1>
     <h2>{Nerd.interest + " is a " + Nerd.everything + " and " + Nerd.everything + " is " + Nerd.shit} </h2>
     </>
